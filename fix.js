61a8713dc0fc2bd5898d290c49dc9c0f84c27ea0
Gilangp <gilangpurnomo0505@gmail.com>
2026-06-18 19:49:48 +0700

    fix

---
+++ fix.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+function walk(dir) {
+  let results = [];
+  const list = fs.readdirSync(dir);
+  list.forEach(file => {
+    file = path.join(dir, file);
+    const stat = fs.statSync(file);
+    if (stat && stat.isDirectory()) {
+      results = results.concat(walk(file));
+    } else {
+      if (file.endsWith('route.ts')) {
+        results.push(file);
+      }
+    }
+  });
+  return results;
+}
+
+const files = walk('./app/api');
+files.forEach(file => {
+  let content = fs.readFileSync(file, 'utf8');
+  if (content.includes('export async function GET') && !content.includes('export const dynamic')) {
+    content = "export const dynamic = 'force-dynamic';\n\n" + content;
+    fs.writeFileSync(file, content);
+    console.log('Fixed:', file);
+  }
+});
+console.log('Done');
